78c1d4a5dde40e1a0c1610f89f08033863c64df4
Toni Alatalo <toni@playsign.net>
2010-02-10 16:15:19 +0200

    docs explaining what this is and why

--- event/component_calls_another.py
+++ event/component_calls_another.py
@@ -1,3 +1,30 @@
+"""
+An example of how event feedback channels work in Circuits,
+and can be used to make a component based event driven app.
+
+This was inspired by a need with inventory services in Naali: 
+
+It would be nice if parts of Naali like inventory impls and their
+users would not be tightly coupled. The service interfaces we have now
+are one solution for that, but another idea would be to just use events.
+
+A problem with events is that how to get a reply to a query event.
+For example when someone wants to know what name an asset uuid has -- 
+with opensim currently assets themselves don't have names but in inventory they do,
+so the viewer gui must query the inventory to be able to show them.
+
+This is a working implementation of such a query event, with two
+separate components (could be modules in Naali), Responder and a
+Requester (could be Inventory and Object edit GUI, for
+example). Requester sends a Query event (would be
+InventoryAssetinfoQuery or so in Naali) and gets the response from the
+decoupled Responder.
+
+The solution here is to use a new feature in Circuits, feedback
+channels. The event type is defined so that the success channel for it
+is called 'query_success'. The return value from the handler in Responder is sent as an event on that channel, so the Requester gets it.
+"""
+
 from circuits import Debugger
 from circuits import Event, Component
 
